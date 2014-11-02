627ef98ad5a4131ad71c4f11820382159a93b582
root <root@localhost.localdomain>
2014-11-02 15:55:07 +0530

    prompt added

--- final.py
+++ final.py
@@ -1,6 +1,13 @@
 import cv2,sys,numpy,random,os,math,select,time
 from threading import Thread
-import sqlite3,datetime
+import sqlite3,datetime,sys
+
+
+def prompt() :
+ sys.stdout.write('\rWatchMen>>  ')
+ sys.stdout.flush()
+
+
 
 fn_dir='faces'
 
@@ -151,31 +158,46 @@ class StartCam(Thread):
 cam1 = StartCam(0)
 cam1.start()
 
-raw_input()
-person = raw_input("Enter Name:  ")
-print "What do you wanna do?"
-choice = raw_input("\t******MENU******\n1. Last Known Location \n2. Track \n3. Notify when Next seen\n\t=>  ")
 
-if (int(choice)==1):
-  conn2 = sqlite3.connect('Watchmen.db')
-  cur2 = conn2.execute("SELECT * from PEOPLE WHERE NAME=:name",{"name":str(person)})
-  rows = cur2.fetchall()
-  index = -1
-  max_time = '0'
-  for row in rows:
-    index = index + 1
-  print rows[index][3]
+choice = 0
+while(choice<4):
+  prompt()
+  print "Press any key  "
+  raw_input()
+  prompt()
+  person = raw_input("Enter Name:  ")
+  prompt()
+  print "What do you wanna do?"
+  choice = raw_input("\t******MENU******\n1. Last Known Location \n2. Track \n3. Notify when Next seen\n4. Exit\n\t=>  ")
+  if (int(choice)==1):
+    conn = sqlite3.connect('Watchmen.db')
+    cur2 = conn.execute("SELECT Max(ID) FROM PEOPLE WHERE NAME='%s';"%(str(person)));
+    max_id = cur2.fetchone()[0]
+    if(max_id != None):
+      cur2 = conn.execute("SELECT LAST_SEEN_TIME, CAMERA_NO from PEOPLE WHERE NAME=:name and ID=:id",{"name":str(person),"id":int(max_id)})
+      need = cur2.fetchone()
+      last_known_time = need[0]
+      prompt()
+      print need[0]
+    else:
+      prompt()
+      print "Sorry! Person not in database."
 
+  if (int(choice)==2):
+    conn2 = sqlite3.connect('Watchmen.db')
+    cur2 = conn2.execute("SELECT * from PEOPLE WHERE NAME=:name",{"name":str(person)})
+    rows = cur2.fetchall()
+    if(rows==None):
+      prompt()
+      print "Sorry! Person not in database."
+    else:
+      prompt()
+      for row in rows:
+        print row[3]
+  
 
-if (int(choice)==2):
-  conn2 = sqlite3.connect('Watchmen.db')
-  cur2 = conn2.execute("SELECT * from PEOPLE WHERE NAME=:name",{"name":str(person)})
-  rows = cur2.fetchall()
-  for row in rows:
-    print row[3]
-     
-
-
+  if (int(choice)==4):
+       sys.exit(0)
 
 
 
